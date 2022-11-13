c89e8681390b2f4aeaaf2a0739808a94eddb7f32
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-13 21:14:32 +0200

    3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,25 +3,32 @@ import Notiflix from 'notiflix';
 
 const refs = {
   form: document.querySelector('.form'),
-  delayRef: document.querySelector('#delay'),
-  stepRef: document.querySelector("#step"),
-  amountRef: document.querySelector('#amount'), 
+  delayRef: document.querySelector('input[name=delay]'),
+  stepRef: document.querySelector("input[name=step]"),
+  amountRef: document.querySelector('input[name=amount]'), 
 };
 
+console.log(refs.form);
 refs.form.addEventListener('submit', createPromise)
 
-let delay = null;
+let startDelay = null;
 let position = 0
-
+const amount  = refs.amountRef.value
+console.log(amount);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notiflix.Notify.success('✅ Fulfilled promise ${position} in ${delay}ms');
-  } else {
-    Notiflix.Notify.failure('❌ Rejected promise ${position} in ${delay}ms');
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      Notiflix.Notify.success('✅ Fulfilled promise ${position} in ${delay}ms');
+    } else {
+      Notiflix.Notify.failure('❌ Rejected promise ${position} in ${delay}ms');
+    }  
+  });
+
+
 }
+
 
 
 
