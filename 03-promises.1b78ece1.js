a22e2fcb9ca0235906fc24b2b865ee97bfb82cdd
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-13 15:58:25 +0000

    Deploying to gh-pages from @ CherednikNatalya/goit-js-hw-09@5838db3022e9f86979d00fd53071821a81b46447 🚀

--- 03-promises.1b78ece1.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".timer__items");console.log(new Date("2030-03-16")),console.log(new Date("2030-03")),console.log(new Date("2018"));const e=setInterval((function(){}),1e3),o=setInterval((function(){}),1e3),n=setInterval((function(){}),1e3);console.log(e),console.log(n),console.log(o);
-//# sourceMappingURL=03-promises.1b78ece1.js.map
--- 03-promises.6864470b.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".timer__items");console.log(new Date("2030-03-16")),console.log(new Date("2030-03")),console.log(new Date("2018"));var e=setInterval((function(){}),1e3),o=setInterval((function(){}),1e3),n=setInterval((function(){}),1e3);console.log(e),console.log(n),console.log(o)}();
-//# sourceMappingURL=03-promises.6864470b.js.map
