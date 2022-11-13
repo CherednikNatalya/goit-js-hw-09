5838db3022e9f86979d00fd53071821a81b46447
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-13 17:58:02 +0200

    478

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,121 +1,29 @@
+import Notiflix from 'notiflix';
+
+
+const refs = {
+  form: document.querySelector('.form'),
+  delayRef: document.querySelector('#delay'),
+  stepRef: document.querySelector("#step"),
+  amountRef: document.querySelector('#amount'), 
+};
+
+refs.form.addEventListener('submit', createPromise)
+
+let delay = null;
+let position = 0
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success('✅ Fulfilled promise ${position} in ${delay}ms');
   } else {
-    // Reject
+    Notiflix.Notify.failure('❌ Rejected promise ${position} in ${delay}ms');
   }
 }
 
 
 
-const timerRef = document.querySelector('.timer__items');
-
-const timer = {
-  intervalId: null,
-  refs: {},
-  notifyOptions: {
-    position: 'center-center',
-    backOverlay: true,
-    clickToClose: true,
-    closeButton: true,
-  },
-  start(rootSelector, deadline) {
-    const delta = deadline.getTime() - Date.now();
-    if (delta <= 0) {
-      Notify.failure(
-        'Вибраний час в минулому, виберіть дату в майбутньому!',
-        this.notifyOptions
-      );
-      return;
-    }
-    Notify.success('Відлік почався', this.notifyOptions);
-    
-    this.getRefs(rootSelector);
-    this.intervalId = setInterval(() => {
-      const diff = deadline.getTime() - Date.now();
-
-      if (diff <= 1000) {
-        clearInterval(this.intervalId);
-        Notify.success('Дедлайн настав!', this.notifyOptions);
-      }
-
-      const data = this.convertMs(diff);
-      //   Object.entries(data).forEach(([name, value]) => {
-      //     this.refs[name].textContent = this.addLeadinZero(value);
-      //   });
-      this.refs.days.textContent = this.addLeadinZero(data.days);
-      this.refs.hours.textContent = this.addLeadinZero(data.hours);
-      this.refs.minutes.textContent = this.addLeadinZero(data.minutes);
-      this.refs.seconds.textContent = this.addLeadinZero(data.seconds);
-    }, 1000);
-  },
-
-  getRefs(rootSelector) {
-    // [...rootSelector.children].forEach(item => {
-    //   const { title } = item.dataset;
-    //   this.refs[title] = item;
-    // });
-    this.refs.days = rootSelector.querySelector('.js-timer__days');
-    this.refs.hours = rootSelector.querySelector('.js-timer__hours');
-    this.refs.minutes = rootSelector.querySelector('.js-timer__minutes');
-    this.refs.seconds = rootSelector.querySelector('.js-timer__seconds');
-  },
-  convertMs(diff) {
-    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-    const minutes = Math.floor(diff / 1000 / 60) % 60;
-    const seconds = Math.floor(diff / 1000) % 60;
-    return { days, hours, minutes, seconds };
-  },
-  addLeadinZero(value) {
-    return String(value).padStart(2, '0');
-  },
-};
-
-// timer.start(timerRef, TIMER_DEADLINE);
-console.log(new Date('2030-03-16'));
-console.log(new Date('2030-03'));
-console.log(new Date('2018'));
-
-const interval1 = setInterval(function () {
-  // body
-}, 1000);
-
-const interval2 = setInterval(function () {
-  // body
-}, 1000);
-
-const interval3 = setInterval(function () {
-  // body
-}, 1000);
-
-console.log(interval1);
-console.log(interval3);
-console.log(interval2);
 
 
-
-// console.log(Date.now());
-// console.log(rootSelector);
-// console.log(delta);
-
-  // const timeDeadline = new Date();
-  // this.intervalId = setInterval(() => {
-  //   const startTime = timeDeadline - Date.now();
-  //   const timeComponents = convertMs(startTime)
-
-  //   const time = convertMs(inputTime);
-  
-  //   if (inputTime < 0) {
-  //     return;
-  //   } else {
-  //     refs.days.textContent = `${days}`;
-  //     refs.hours.textContent = `${hours}`;
-  //     refs.minutes.textContent = `${minutes}`;
-  //     refs.seconds.textContent = `${seconds}`;
-  //   }
-  //   if (time === 0) {
-  //     clearInterval(intervalId);
-  //   };
-  // }, 1000);
