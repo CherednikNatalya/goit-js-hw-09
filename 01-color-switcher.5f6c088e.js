86a7ff5703a13c8564e947d474296df379fe8b95
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-18 14:51:03 +0000

    Deploying to gh-pages from @ CherednikNatalya/goit-js-hw-09@ff71e8f3b163a06732adf037339683b23e20bb05 🚀

--- 01-color-switcher.5f6c088e.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("[data-start]"),stoptBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};let e=null;t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0})),t.stoptBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.disabled=!1,t.stoptBtn.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.5f6c088e.js.map
